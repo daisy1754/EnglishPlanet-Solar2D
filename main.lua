af38dc7b6d2d38d2a43dec10a6e6d381712d3191
daisy1754 <d-part@hotmail.co.jp>
2019-12-28 22:11:04 -0800

    Add background images

--- main.lua
+++ main.lua
@@ -4,4 +4,19 @@
 --
 -----------------------------------------------------------------------------------------
 
--- Your code here
+local background = display.newImageRect( "images/bg_blue.png", display.contentWidth, display.contentHeight )
+background.x = display.contentCenterX
+background.y = display.contentCenterY
+
+display.setDefault("textureWrapX", "repeat")
+display.setDefault("textureWrapY", "repeat")
+local starts = display.newRect(display.contentCenterX, display.contentCenterY, display.contentWidth, display.contentHeight)
+starts.fill = {type = "image", filename = "images/bg_stars.png" }
+local function animateBackground()
+    transition.to( starts.fill, { time=30000, x=1, y=-0.6, delta=true, onComplete=animateBackground } )
+end
+animateBackground()
+
+local planet = display.newImageRect( "images/planet.png", display.contentWidth / 2, display.contentWidth / 2 )
+planet.x = display.contentCenterX
+planet.y = display.contentCenterY
