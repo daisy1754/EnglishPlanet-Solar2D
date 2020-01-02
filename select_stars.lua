9bf14372f1e6548aeccad4b5240db74392042859
daisy1754 <d-part@hotmail.co.jp>
2020-01-01 21:38:07 -0800

    Add motion for swiping stars

--- select_stars.lua
+++ select_stars.lua
@@ -18,6 +18,17 @@ local starNames = {
     "jikan",
     "tabemono"
 }
+local starInfo = {
+    { name = "うみの いきもの", image = "umi"},
+    { name = "てんき", image = "tenki"},
+    { name = "かぞく", image = "kazoku"},
+    { name = "こうどう", image = "koudou"},
+    { name = "りくの いきもの", image = "riku"},
+    { name = "のりもの", image = "norimono"},
+    { name = "もの", image = "mono"},
+    { name = "じかん", image = "jikan"},
+    { name = "たべもの", image = "tabemono"},
+}
 local angles = {
     0,
     65,
@@ -29,6 +40,31 @@ local angles = {
     -95,
     -65
 }
+
+local swipeStar
+local stars = {}
+local currentOffset = 1
+
+local function touchHandler(event)
+    local swipeLength = math.abs(event.x - event.xStart) 
+    print(event.phase, swipeLength)
+
+    local t = event.target
+    local phase = event.phase
+
+    if (phase == "began") then
+
+    elseif (phase == "moved") then
+
+    elseif (phase == "ended" or phase == "cancelled") then
+
+        if (event.xStart > event.x and swipeLength > 50) then 
+            swipeStar(1)
+        elseif (event.xStart < event.x and swipeLength > 50) then 
+            swipeStar(-1)
+        end
+    end
+end
 
 function scene:create( event ) 
 	local sceneGroup = self.view
@@ -59,36 +95,58 @@ function scene:create( event )
         stars.y = centerY
     end
 
+    local function getStarPosition(angle) 
+        return {
+            x = display.contentCenterX + unitX * 400 * math.cos(math.rad(angle)),
+            y = display.contentCenterY - unitY * 100 + 0.7 * unitX * 400 * math.sin(math.rad(angle))
+        }
+    end
+
+    local function getStarZoom(angle) 
+        if angle == 90 then
+            return 1.6
+        elseif math.sin(math.rad(angle)) < 0 then    
+            return 1 + math.sin(math.rad(angle)) * 5 / 10
+        else
+            return 1 + math.sin(math.rad(angle)) * 2 / 10
+        end
+    end
+
     local function initStars()
-        local stars = {}
-        for i, starName in ipairs(starNames) do
-            local star = display.newImageRect(mainGroup, "images/stars/" .. starName .. ".png", unitX * 300, unitX * 300)
+        stars = {}
+        for i, info in ipairs(starInfo) do
+            local star = display.newImageRect(mainGroup, "images/stars/" .. info.image .. ".png", unitX * 300, unitX * 300)
             local angle = angles[i] + 90
-            star.x = display.contentCenterX + unitX * 400 * math.cos(math.rad(angle))
-            star.y = display.contentCenterY - unitY * 100 + 0.7 * unitX * 400 * math.sin(math.rad(angle))
-            local zoom 
-            if i == 1 then
-                zoom = 1.6
-            elseif math.sin(math.rad(angle)) < 0 then    
-                zoom = 1 + math.sin(math.rad(angle)) * 5 / 10
-            else
-                zoom = 1 + math.sin(math.rad(angle)) * 2 / 10
-            end
-            star:scale(zoom, zoom)
+            local position = getStarPosition(angle)
+            star.x = position.x
+            star.y = position.y
             stars[#stars + 1] = star
-            print(string.format("%s %d %f", starName, angle, math.sin(math.rad(angle))))
         end
-        for i=1,4 do
-            bgGroup:insert(stars[5-i])
+        local function adjustZoom() 
+            for i, star in ipairs(stars) do
+                local angle = angles[i] + 90
+                print(i, angle, getStarZoom(angle))
+                star:scale(getStarZoom(angle), getStarZoom(angle))
+            end
         end
+        local function adjustZIndex()
+            for i=1,4 do
+                bgGroup:insert(stars[5-i])
+            end
+            for i=1,4 do
+                bgGroup:insert(stars[5+i])
+            end
+        end
+        adjustZoom()
+        adjustZIndex()
 
         local starNameFrame = display.newRect( mainGroup, centerX, 
-            stars[1].y + stars[1].contentHeight / 2 + unitX * 120,  unitX * 600, unitX * 120 )
+            stars[1].y + stars[1].contentHeight / 2 + unitX * 140,  unitX * 600, unitX * 120 )
         starNameFrame:setFillColor( 1, 1, 1, 0.8 )
 		
 		local titleText = display.newText({
 			parent = mainGroup,
-			text = "たべもの プラネット",     
+			text = starInfo[1].name .. " プラネット",     
 			x = starNameFrame.x,
 			y = starNameFrame.y,
 			width = starNameFrame.contentWidth,
@@ -96,7 +154,35 @@ function scene:create( event )
 			fontSize = 24,
 			align = "center"
 		})
-		titleText:setFillColor( 0 )
+        titleText:setFillColor( 0 )
+        
+        swipeStar = function(offset)
+            currentOffset = (currentOffset - offset - 1) % #angles + 1
+            local transitionDuration = 200
+            for i, star in ipairs(stars) do
+                local nextAngle = angles[(i + offset - 1) % #angles + 1] + 90
+                local nextPosition = getStarPosition(nextAngle)
+                local nextZoom = getStarZoom(nextAngle)
+                transition.to( star, { 
+                    time=transitionDuration,
+                    transition=easing.inOutQuad, 
+                    xScale=nextZoom,
+                    yScale=nextZoom,
+                    x=nextPosition.x,
+                    y=nextPosition.y,
+                } )
+            end
+            local function onDone()
+                local tmp = {}
+                for i, star in ipairs(stars) do
+                    tmp[(i + offset - 1) % #stars + 1] = star
+                end
+                stars = tmp
+                adjustZIndex()
+                titleText.text = starInfo[currentOffset].name .. " プラネット"
+            end
+            timer.performWithDelay(transitionDuration, onDone, 1)
+        end
     end
 
     local function initPlayer()
@@ -126,6 +212,7 @@ function scene:create( event )
     initBackground()
     initStars()
     initPlayer()
+    Runtime:addEventListener("touch", touchHandler)
 end
 
 function scene:show( event )
@@ -157,6 +244,7 @@ function scene:hide( event )
 end
 
 function scene:destroy( event )
+    Runtime:removeEventListener("touch", touchHandler)
 
 	local sceneGroup = self.view
 	-- Code here runs prior to the removal of scene's view
